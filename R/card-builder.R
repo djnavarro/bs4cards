989fadbe763c644a95e0808b9b3dc20b4fd417a7
Danielle Navarro <d.navarro@unsw.edu.au>
2021-03-27 17:14:01 +1100

    moves card_group() into card-builder.R

--- R/card-builder.R
+++ R/card-builder.R
@@ -41,3 +41,19 @@ card <- function(title, text, image, image_align = "top", col_spec = "col-12 col
 
 
 
+#' Create a deck of cards
+#'
+#' @param cards List of cards
+#'
+#' @return sdfgdfg
+#' @export
+#'
+#' @examples
+card_group <- function(cards) {
+  cards %>%
+    htmltools::div(class = "card-group") %>%
+    htmltools::div(class = "row") %>%
+    htmltools::div(class = "container")
+}
+
+
