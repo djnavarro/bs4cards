c72a0c317daf34190884b68cfe546bfaa67c61cb
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-13 15:10:35 +1000

    adds use_strapless

--- R/strapless.R
+++ R/strapless.R
@@ -1,10 +1,24 @@
 
 
-#' Locate the strapless.css file
+#' Use the strapless css library
+#'
+#' @param destdir directory to install the strapless library
+#' @param source set source = "local" to use the cached copy, source = "github" to download
 #'
 #' @return path to file
 #' @export
 #'
-strapless <- function() {
-  system.file("extdata", "strapless.css", package = "bs4cards")
+use_strapless <- function(destdir = ".", source = "local") {
+  destfile <- fs::path(destdir, "strapless.css")
+  if(source == "local") {
+    path <- system.file("extdata", "strapless.css", package = "bs4cards")
+    fs::file_copy(path, destfile)
+    return(invisible(NULL))
+  }
+  if(source == "github") {
+    url <- "https://github.com/djnavarro/strapless/releases/download/0.0.0.9000/strapless.css"
+    download.file(url, destfile)
+    return(invisible(NULL))
+  }
+  stop('source must be "local" or "github"', call. = FALSE)
 }
