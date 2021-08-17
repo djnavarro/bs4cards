c6533b6fa780f12121c3dfe06f466c5001b27b70
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-17 17:16:54 +1000

    adds blurb and date columns to example data

--- R/thumbnails_data.R
+++ R/thumbnails_data.R
@@ -1,13 +1,13 @@
 
 #'@export
 galleries <- tibble::tribble(
-  ~long_name,               ~short_name,
-  "Ash Cloud and Blood",    "ash",
-  "Ghosts on Marble Paper", "ghosts",
-  "Ice Floes",              "ice-floes",
-  "Native Flora",           "native-flora",
-  "Silhouettes",            "silhouettes",
-  "Track Marks",            "track-marks",
-  "Viewports",              "viewports"
+  ~long_name,               ~short_name,    ~date,        ~blurb,
+  "Ash Cloud and Blood",    "ash",          "2021-07-17", "Simulated watercolours in ggplot2 using the transparent polygon method",
+  "Ghosts on Marble Paper", "ghosts",       "2021-01-09", "Abstract landscapes generated using ambient",
+  "Ice Floes",              "ice-floes",    "2021-01-19", "Abstract icebergs generated using ambient",
+  "Native Flora",           "native-flora", "2021-02-10", "Collections of small flametrees that resemble branching plants",
+  "Silhouettes",            "silhouettes",  "2021-01-24", "Trees generated in flametree with random walk backgrounds",
+  "Track Marks",            "track-marks",  "2020-09-20", "Voronoi tessellations with seed points sampled from an iterated function system",
+  "Viewports",              "viewports",    "2021-02-28", "Simple masks with ggfx using flametree-generated textures"
 )
 
