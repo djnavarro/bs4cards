1b6bad83b2fa642319a82f8218f92987c3df29d0
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-18 23:15:23 +1000

    adds user-facing border and rounding parameters

--- R/cards.R
+++ R/cards.R
@@ -12,16 +12,31 @@
 #' @param gutter Spacing between adjacent cards (integer between 0 and 5)
 #' @param breakpoint Position to insert breaks for horizontal or inset cards, interpreted as proportion of card allocated to label
 #' @param colour Colour applied to the card (interpretation depends on layout)
+#' @param border_width Width of card border
+#' @param border_colour Colour of card border
+#' @param rounding Amount of rounding on card corners
 #'
 #' @return A "shiny.tag" object
 #' @export
 #'
-cards <- function(data, title = NULL, text = NULL, image = NULL, link = NULL,
-                  footer = NULL, width = "medium",
-                  layout = "label-below", padding = 0, gutter = 1,
-                  breakpoint = NULL, colour = "#ffffffaa") {
+cards <- function(data,
+                  title = NULL,
+                  text = NULL,
+                  image = NULL,
+                  link = NULL,
+                  footer = NULL,
+                  width = "medium",
+                  layout = "label-below",
+                  padding = 0,
+                  gutter = 1,
+                  breakpoint = NULL,
+                  colour = "#ffffffaa",
+                  border_width = "1px",
+                  border_colour = "#808080",
+                  rounding = "1rem"
+                  ) {
 
-  card_list <-data %>%
+  card_list <- data %>%
     dplyr::transmute(
       title  = {{title}},
       text   = {{text}},
@@ -32,10 +47,15 @@ cards <- function(data, title = NULL, text = NULL, image = NULL, link = NULL,
       padding = {{padding}},
       gutter = {{gutter}},
       breakpoint = {{breakpoint}},
-      colour = {{colour}}
-    ) %>%
+      colour = {{colour}},
+      border_width = {{border_width}},
+      border_colour = {{border_colour}},
+      rounding = {{rounding}}
+    )
+
+  card_list <- card_list %>%
     purrr::transpose() %>%
-    purrr::map(purrr::lift_dl(make_card))
+    purrr::map(make_card_dots)
 
   collate <- function(...) {
     row_margins <- paste(
@@ -55,10 +75,12 @@ cards <- function(data, title = NULL, text = NULL, image = NULL, link = NULL,
 
 make_card <- function(title = NULL, text = NULL, image = NULL, link = NULL,
                       footer = NULL, header = NULL, layout,
-                      padding, gutter, breakpoint = NULL, colour) {
+                      padding, gutter, breakpoint = NULL, colour,
+                      border_width, border_colour, rounding
+                      ) {
 
-  border <- c(width = "1px", colour = "#808080", style = "solid")
-  radius <- "1rem";
+  border <- c(width = border_width, colour = border_colour, style = "solid")
+  radius <- rounding;
 
   if(layout == "label-below" | layout == "label-above") {
     card <- make_card_vertical(
@@ -90,4 +112,6 @@ make_card <- function(title = NULL, text = NULL, image = NULL, link = NULL,
     return(card)
   }
 }
+
+make_card_dots <- purrr::lift_dl(make_card)
 
