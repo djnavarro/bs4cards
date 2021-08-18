e94ba61de6224c8ae5074ec8829f3b19236d3331
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-18 21:36:04 +1000

    adds tidy corners and borders

--- R/cards.R
+++ R/cards.R
@@ -53,191 +53,41 @@ cards <- function(data, title = NULL, text = NULL, image = NULL, link = NULL,
 
 
 
-# arrange the pieces into a card ------------------------------------------
-
 make_card <- function(title = NULL, text = NULL, image = NULL, link = NULL,
                       footer = NULL, header = NULL, layout,
                       padding, gutter, breakpoint = NULL, colour) {
 
-  title  <- make_title(title)
-  text   <- make_text(text)
-  image  <- make_image(image, layout)
-  footer <- make_footer(footer)
-
-  if(!is.null(link)) {
-    title <- htmltools::a(title, href = link)
-    image <- htmltools::a(image, href = link)
-  }
+  border <- c(width = "1px", colour = "#808080", style = "solid")
+  radius <- "1rem";
 
   if(layout == "label-below" | layout == "label-above") {
-    card <- make_card_vertical(image, title, text, footer, layout, padding, gutter, colour)
+    card <- make_card_vertical(
+      title, text, image, link,
+      footer, header, layout,
+      padding, gutter, breakpoint,
+      colour, border, radius
+    )
     return(card)
   }
 
   if(layout == "label-right" | layout == "label-left") {
-    card <- make_card_horizontal(image, title, text, footer, layout, padding, gutter, breakpoint, colour)
+    card <- make_card_horizontal(
+      title, text, image, link,
+      footer, header, layout,
+      padding, gutter, breakpoint,
+      colour, border, radius
+    )
     return(card)
   }
 
   if(layout == "inset-bottom" | layout == "inset-top") {
-    card <- make_card_inset(image, title, text, footer, layout, padding, gutter, breakpoint, colour)
+    card <- make_card_inset(
+      title, text, image, link,
+      footer, header, layout,
+      padding, gutter, breakpoint,
+      colour, border, radius
+    )
     return(card)
   }
 }
-
-
-# card builders for different layout types --------------------------------
-
-
-make_card_vertical <- function(image, title, text, footer, layout, padding, gutter, colour) {
-
-  body <- htmltools::div(
-    class = paste0("card-body justify-content-end border rounded m-0 p-", padding),
-    style = paste0("visibility: visible; background-color: ", colour),
-    title, text
-  )
-
-  card_class <- paste0("card bg-transparent border-0 m-0 p-", gutter)
-
-  if(layout == "label-below") {
-    card <- htmltools::div(
-      class = card_class,
-      image, body, footer
-    )
-  }
-
-  if(layout == "label-above") {
-    card <- htmltools::div(
-      class = card_class,
-      body, image, footer
-    )
-  }
-
-  return(card)
-
-}
-
-
-make_card_horizontal <- function(image, title, text, footer, layout, padding, gutter, breakpoint, colour) {
-
-  # the default for horizontal cards is a wide label, presuming the common
-  # use case for horizontal cards is for people to supply a descriptive blurb
-  if(is.null(breakpoint)) breakpoint <- 2/3
-
-  label_cols <- round(breakpoint * 12)
-  image_cols <- 12 - label_cols
-
-  label_class <- paste0("col-sm-", label_cols)
-  image_class <- paste0("col-sm-", image_cols)
-
-  if(layout == "label-right") {
-    lhs <- htmltools::div(class = image_class, image)
-    rhs <- htmltools::div(class = label_class, title, text, style = "background-color: #00000000;")
-    row <- htmltools::div(class = "row no-gutters", lhs, rhs)
-    core <- htmltools::div(row)
-  }
-
-  if(layout == "label-left") {
-    lhs <- htmltools::div(class = label_class, title, text, style = "background-color: #00000000;")
-    rhs <- htmltools::div(class = image_class, image)
-    row <- htmltools::div(class = "row no-gutters", lhs, rhs)
-    core <- htmltools::div(row)
-  }
-
-  inner <- htmltools::div(
-    class = paste0("card-body border rounded m-0 p-", padding, " col-12"),
-    style = paste0("visibility: visible; background-color: ", colour),
-    core
-  )
-
-  card <- htmltools::div(
-    class = paste0("card bg-transparent border-0 m-0 p-", gutter),
-    inner, footer
-  )
-
-  return(card)
-}
-
-
-
-make_card_inset <- function(image, title, text, footer, layout, padding, gutter, breakpoint, colour) {
-
-  # the default breakpoint for inset cards is thin, as it is presumed the
-  # typical use case for inset cards is to provide a small floating caption/title
-  if(is.null(breakpoint)) breakpoint <- 1/6
-
-  label_height <- round(breakpoint * 100)
-  label_offset <- ifelse(layout == "inset-bottom", 100 - label_height, 0)
-
-  overlay <- htmltools::div(
-    class = "card-img-overlay p-0 m-0",
-    style = paste0(
-      "background-color: ", colour, ";",
-      "height: ", label_height, "%;",
-      "position: absolute;",
-      "top: ", label_offset, "%;"
-    ),
-    title, text
-  )
-
-  inner <- htmltools::div(
-    style = "position: relative",
-    image, overlay
-  )
-
-  card <- htmltools::div(
-    class = paste0("card bg-transparent border-0 m-0 p-", gutter),
-    inner, footer
-  )
-
-  return(card)
-}
-
-# wrapper functions for small html pieces ---------------------------------
-
-make_title <- function(title) {
-  if(!is.null(title)) {
-    htmltools::h5(
-      title,
-      class = "card-title px-3",
-      style = "margin-top:1rem; margin-bottom:1rem"
-    )
-  }
-}
-
-make_text <- function(text) {
-  if(!is.null(text)) htmltools::p(text, class = "card-text px-3 pb-3")
-}
-
-make_image <- function(image, layout) {
-  if(is.null(image)) return(NULL)
-  if(layout == "label-above") return(htmltools::img(src = image, class = "card-img-bottom"))
-  if(layout == "label-below") return(htmltools::img(src = image, class = "card-img-top"))
-  return(htmltools::img(src = image, class = "card-img"))
-}
-
-make_footer <- function(footer) {
-  if(!is.null(footer)) {
-    htmltools::div(footer, class = "card-footer small text-muted h-auto px-3 py-auto border-0") #bg-transparent")
-  }
-}
-
-make_width <- function(width) {
-  if(width == "narrow") return("col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2")
-  if(width == "medium") return("col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3")
-  if(width == "wide") return("col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4")
-  if(width == "very-wide") return("col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6")
-  return(width)
-}
-
-make_row_cols <- function(width){
-  if(width == "narrow") return("row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6")
-  if(width == "medium") return("row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4")
-  if(width == "wide") return("row-cols-1 row-cols-md-2 row-cols-lg-3")
-  if(width == "very-wide") return("row-cols-1 row-cols-lg-2")
-  return(width)
-}
-
-
-
 
---
+++ R/css-helpers.R
@@ -0,0 +1,112 @@
+
+# helpers to generate css -----------------------------------------
+
+make_row_cols <- function(width){
+  if(width == "narrow") return("row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6")
+  if(width == "medium") return("row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4")
+  if(width == "wide") return("row-cols-1 row-cols-md-2 row-cols-lg-3")
+  if(width == "very-wide") return("row-cols-1 row-cols-lg-2")
+  return(width)
+}
+
+card_classes <- function(gutter) {
+  paste0("card bg-transparent m-0 p-", gutter, " border-0")
+}
+
+image_classes <- function(layout) {
+  card_type <- "card-img"
+  if(layout == "label-above") card_type <- "card-img-bottom"
+  if(layout == "label-below") card_type <- "card-img-top"
+  paste0(card_type)
+}
+
+image_style <- function(corners, border) {
+  paste0(
+    "border-style:", border[["style"]], "; ",
+    "border-color:", border[["colour"]], "; ",
+    "border-width:", border[["width"]], "; ",
+    corners[["image"]]
+  )
+}
+
+footer_classes <- function() {
+  paste0("card-footer small text-muted h-auto px-3 py-auto")
+}
+
+footer_style <- function(corners, border) {
+  paste0(
+    "border-style:", border[["style"]], "; ",
+    "border-color:", border[["colour"]], "; ",
+    "border-width:", border[["width"]], "; ",
+    corners[["footer"]]
+  )
+}
+
+card_style <- function(corners) {
+  corners[["card"]]
+}
+
+body_vertical_classes <- function(padding) {
+  paste0("card-body justify-content-end m-0 p-", padding)
+}
+
+body_vertical_style <- function(colour, corners, border) {
+  paste0(
+    "visibility: visible; ",
+    "background-color: ", colour, "; ",
+    "border-style:", border[["style"]], "; ",
+    "border-color:", border[["colour"]], "; ",
+    "border-width:", border[["width"]], "; ",
+    corners[["label"]]
+  )
+}
+
+label_horizontal_classes <- function(breakpoint) {
+  paste0("col-sm-", round(breakpoint * 12))
+}
+
+label_horizontal_style <- function(colour, corners, border) {
+  paste(
+    "background-color:", colour, ";",
+    "border-style:", border[["style"]], "; ",
+    "border-color:", border[["colour"]], "; ",
+    "border-width:", border[["width"]], "; ",
+    corners[["label"]]
+  )
+}
+
+image_horizontal_classes <- function(breakpoint) {
+  paste0("col-sm-", 12 - round(breakpoint * 12))
+}
+
+image_horizontal_style <- function(corners) { # border already styled by image
+  corners[["image"]]
+}
+
+
+body_inset_style <- function(corners) {
+  paste(
+    "position: relative;",
+    corners[["label"]]
+  )
+}
+
+label_inset_classes <- function() {
+  "card-img-overlay p-0 m-0"
+}
+
+label_inset_style <- function(colour, corners, border, breakpoint, layout) {
+  height <- round(breakpoint * 100)
+  paste0(
+    "background-color: ", colour, ";",
+    "height: ", height, "%;",
+    "position: absolute;",
+    "top: ", ifelse(layout == "inset-bottom", 100 - height, 0), "%;",
+    "border-style:", border[["style"]], "; ",
+    "border-color:", border[["colour"]], "; ",
+    "border-width:", border[["width"]], "; ",
+    corners[["label"]]
+  )
+}
+
+
