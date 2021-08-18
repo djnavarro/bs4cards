eab3b22a6925d86840737401e80f3c1145590d00
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-18 14:38:38 +1000

    use_strapless() uses base functions

--- R/strapless.R
+++ R/strapless.R
@@ -9,15 +9,15 @@
 #' @export
 #'
 use_strapless <- function(destdir = ".", source = "local") {
-  destfile <- fs::path(destdir, "strapless.css")
+  destfile <- file.path(destdir, "strapless.css")
   if(source == "local") {
     path <- system.file("extdata", "strapless.css", package = "bs4cards")
-    fs::file_copy(path, destfile, overwrite = TRUE)
+    file.copy(path, destfile, overwrite = TRUE)
     return(invisible(NULL))
   }
   if(source == "github") {
     url <- "https://github.com/djnavarro/strapless/releases/download/0.0.0.9000/strapless.css"
-    download.file(url, destfile)
+    utils::download.file(url, destfile)
     return(invisible(NULL))
   }
   stop('source must be "local" or "github"', call. = FALSE)
