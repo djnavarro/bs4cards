8339fd77583233e05563c918769fd038ce7a083d
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-28 10:20:28 +1000

    validators moved to distinct file

--- R/build-card-data.R
+++ R/build-card-data.R
@@ -19,7 +19,7 @@ build_card_data <- function(cardspec, n) {
   # check that valid "card data" has been input and replace NULL with NA
   data_inputs <- c("title", "text", "image", "link", "footer", "header", "tags")
   for(input in data_inputs) {
-    check_valid_input_length(cardspec[[input]], n, input)
+    check_valid_veclength(cardspec[[input]], n, input)
     cardspec[[input]] <- cardspec[[input]] %||% NA_character_
   }
 
@@ -42,72 +42,6 @@ build_card_data <- function(cardspec, n) {
   return(carddata)
 }
 
-is_scalar_numeric <- function(x) {
-  is_scalar_double(x) || is_scalar_integer(x)
-}
-
-check_valid_layout <- function(layout) {
-  err <- 'unrecognised value for layout'
-  if(!is_scalar_character(layout)) abort(err)
-  if(layout %in% c("label-above", "label-below", "label-left",
-                   "label-right", "inset-top", "inset-bottom", "label-only",
-                   "image-only")) return(NULL)
-  abort(err)
-}
-
-check_valid_width <- function(width) {
-  if(is_scalar_numeric(width) && width %in% 1:5) return(NULL)
-  abort("width must be an integer between 1 and 5")
-}
-
-check_valid_padding <- function(padding) {
-  if(is_scalar_numeric(padding) && padding %in% 0:5) return(NULL)
-  abort("padding must be an integer between 0 and 5")
-}
-
-check_valid_spacing <- function(gutter) {
-  err <- "spacing must be an integer between 0 and 5"
-  if(is_scalar_numeric(gutter) && gutter %in% 0:5) return(NULL)
-  abort(err)
-}
-
-check_valid_breakpoint <- function(breakpoint) {
-  err <- "breakpoint must be an integer between 1 and 5"
-  if(is_scalar_numeric(breakpoint) && breakpoint %in% 1:5) return(NULL)
-  abort(err)
-}
-
-check_valid_border_width <- function(x, n) {
-  if(is_null(x)) abort("border_width cannot be NULL")
-  check_valid_input_length(x, n, "border_width")
-}
-
-
-check_valid_rounding <- function(x, n) {
-  if(is_null(x)) abort("rounding cannot be NULL")
-  check_valid_input_length(x, n, "rounding")
-}
-
-
-check_valid_input_length <- function(x, n, var) {
-  err <- paste(
-    var, "must be vector of length 1 or equal to the number of cards"
-  )
-  if(is_null(x)) return(NULL)
-  if(length(x) == 1) return(NULL)
-  if(length(x) == n) return(NULL)
-  abort(err)
-}
-
-# these need to be more precise later...
-check_valid_colour <- function(x, n) {
-  if(is_null(x)) abort("colour cannot be NULL")
-  check_valid_input_length(x, n, "colour")
-}
-check_valid_border_colour <- function(x, n) {
-  if(is_null(x)) abort("border_colour cannot be NULL")
-  check_valid_input_length(x, n, "border_colour")
-}
 
 # if the user only specifies one of the two basic card parts
 # (image & label), require that the layout value be appropriate
---
+++ R/validators.R
@@ -0,0 +1,85 @@
+
+# general purpose functions -----------------------------------------------
+
+
+check_valid_veclength <- function(x, n, var) {
+  err <- paste(
+    var, "must be vector of length 1 or equal to the number of cards"
+  )
+  if(is_null(x)) return(NULL)
+  if(length(x) == 1) return(NULL)
+  if(length(x) == n) return(NULL)
+  abort(err)
+}
+
+is_scalar_numeric <- function(x) {
+  is_scalar_double(x) || is_scalar_integer(x)
+}
+
+
+# layout validator --------------------------------------------------------
+
+
+check_valid_layout <- function(layout) {
+  err <- 'unrecognised value for layout'
+  if(!is_scalar_character(layout)) abort(err)
+  if(layout %in% c("label-above", "label-below", "label-left",
+                   "label-right", "inset-top", "inset-bottom", "label-only",
+                   "image-only")) return(NULL)
+  abort(err)
+}
+
+
+# validators for size-related parameters ----------------------------------
+
+
+check_valid_width <- function(width) {
+  if(is_scalar_character(width)) return(NULL)
+  if(is_scalar_numeric(width) && width %in% 1:5) return(NULL)
+  abort("width must be an integer between 1 and 5")
+}
+
+check_valid_padding <- function(padding) {
+  if(is_scalar_numeric(padding) && padding %in% 0:5) return(NULL)
+  abort("padding must be an integer between 0 and 5")
+}
+
+check_valid_spacing <- function(gutter) {
+  err <- "spacing must be an integer between 0 and 5"
+  if(is_scalar_numeric(gutter) && gutter %in% 0:5) return(NULL)
+  abort(err)
+}
+
+check_valid_breakpoint <- function(breakpoint) {
+  err <- "breakpoint must be an integer between 1 and 5"
+  if(is_scalar_numeric(breakpoint) && breakpoint %in% 1:5) return(NULL)
+  abort(err)
+}
+
+check_valid_border_width <- function(x, n) {
+  if(is_null(x)) abort("border_width cannot be NULL")
+  check_valid_veclength(x, n, "border_width")
+}
+
+
+check_valid_rounding <- function(x, n) {
+  if(is_null(x)) abort("rounding cannot be NULL")
+  check_valid_veclength(x, n, "rounding")
+}
+
+
+# validators for colour parameters ----------------------------------------
+
+
+# these need to be more precise later...
+check_valid_colour <- function(x, n) {
+  if(is_null(x)) abort("colour cannot be NULL")
+  check_valid_veclength(x, n, "colour")
+}
+
+check_valid_border_colour <- function(x, n) {
+  if(is_null(x)) abort("border_colour cannot be NULL")
+  check_valid_veclength(x, n, "border_colour")
+}
+
+
